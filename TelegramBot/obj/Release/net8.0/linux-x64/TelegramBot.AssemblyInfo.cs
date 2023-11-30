0731fb1c157d874e69e659d9c2c7bfeb0fd35849
Dchan-gmd <ghost0910@mail.ru>
2023-11-30 15:17:48 +0300

    try fix images

--- TelegramBot/obj/Release/net8.0/linux-x64/TelegramBot.AssemblyInfo.cs
+++ TelegramBot/obj/Release/net8.0/linux-x64/TelegramBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a371123e3f2eba6d62bd9648918e754fcf5dc7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd64215a4222e818beec7065777d911c770f33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
